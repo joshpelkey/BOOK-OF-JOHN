49bdd0fd48f607b0d84cd2199d64c8902f00bd5f
Josh Pelkey <joshpelkey@gmail.com>
2022-12-12 22:29:11 -0500

    Update gm.py

--- gm.py
+++ gm.py
@@ -2,6 +2,7 @@
 import random
 import psutil
 import time
+import datetime
 import openai
 import math
 from slack_sdk.webhook import WebhookClient
@@ -25,39 +26,188 @@ uptime_minutes = math.floor(uptime // 60)
 uptime %= 60
 uptime_seconds = math.floor(uptime)
 
-# get a random number
-random_num = random.randint(5,1000)
+disk_usage = psutil.disk_usage("/")
 
-# craft a prompt with some placeholder
-placeholder = "Find something cool that happened xxx years ago. Tell me about it in 100 words or less and then give me a thought provoking question at the end."
-prompt_text = placeholder.replace("xxx", str(random_num))
+# let's do something different each day of the week
+day = datetime.datetime.now().weekday()
+
+if day == 0:
+    # today is monday 
+    # monday's are hard. work hard. let's start with something motivating
+    quote = "Share a motivational quote and attribute its author."
+    question = "Tell me an uplifting story about success that is non-fiction. Use 250 words or less."
+
+    # Ask ChatGPT your question
+    quote_response = openai.Completion.create(
+        engine="text-davinci-002",
+        prompt=quote,
+        temperature=0.5,
+        max_tokens=1024
+    )
 
 
-# Ask ChatGPT your question
-chat_response = openai.Completion.create(
-    engine="text-davinci-002",
-    prompt=prompt_text,
-    temperature=0.5,
-    max_tokens=1024
-)
+    # Ask ChatGPT your question
+    question_response = openai.Completion.create(
+        engine="text-davinci-002",
+        prompt=question,
+        temperature=0.5,
+        max_tokens=1024
+    )
+
+elif day == 1:
+    # today is tuesday
+    # tuesday's are for getting shit done. more to come...
+    quote = "Share a quote about hard work and attribute its author."
+    question = "Tell me in 250 words about a person who has contributed greatly to society. What were their works? Use 250 words or less."
+
+    # Ask ChatGPT your question
+    quote_response = openai.Completion.create(
+        engine="text-davinci-002",
+        prompt=quote,
+        temperature=0.5,
+        max_tokens=1024
+    )
+
+
+    # Ask ChatGPT your question
+    question_response = openai.Completion.create(
+        engine="text-davinci-002",
+        prompt=question,
+        temperature=0.5,
+        max_tokens=1024
+    )
+elif day == 2:
+    # today is wednesday
+    # wednesday's are also hard. perservere.
+    quote = "Share a quote about perserverance and attribute its author."
+    question = "Tell me about a complex topic but explain it in simple terms."
+
+    # Ask ChatGPT your question
+    quote_response = openai.Completion.create(
+        engine="text-davinci-002",
+        prompt=quote,
+        temperature=0.5,
+        max_tokens=1024
+    )
+
+
+    # Ask ChatGPT your question
+    question_response = openai.Completion.create(
+        engine="text-davinci-002",
+        prompt=question,
+        temperature=0.5,
+        max_tokens=1024
+    )
+elif day == 3:
+    # today is thursday
+    # on thursday, there is light
+    quote = "Share a inspirational quote and attribute its author."
+    question = "Give me some good advice on raising a son, as a father. Use 250 words or less."
+
+    # Ask ChatGPT your question
+    quote_response = openai.Completion.create(
+        engine="text-davinci-002",
+        prompt=quote,
+        temperature=0.5,
+        max_tokens=1024
+    )
+
+
+    # Ask ChatGPT your question
+    question_response = openai.Completion.create(
+        engine="text-davinci-002",
+        prompt=question,
+        temperature=0.5,
+        max_tokens=1024
+    )
+elif day == 4:
+    # today is friday
+    # friday funday
+    quote = "Share a quote about enjoying life and attribute its author."
+    question = "Tell me a neat fact about something in the music industry. Use 250 words or less."
+
+    # Ask ChatGPT your question
+    quote_response = openai.Completion.create(
+        engine="text-davinci-002",
+        prompt=quote,
+        temperature=0.5,
+        max_tokens=1024
+    )
+
+
+    # Ask ChatGPT your question
+    question_response = openai.Completion.create(
+        engine="text-davinci-002",
+        prompt=question,
+        temperature=0.5,
+        max_tokens=1024
+    )
+elif day == 5:
+    # today is saturday 
+    # saturdays are boss
+    quote = "Share a quote about leadership and attribute its author."
+    question = "Tell me about an interesting but lesser-known hobby or sport in 250 words or less."
+
+    # Ask ChatGPT your question
+    quote_response = openai.Completion.create(
+        engine="text-davinci-002",
+        prompt=quote,
+        temperature=0.5,
+        max_tokens=1024
+    )
+
+
+    # Ask ChatGPT your question
+    question_response = openai.Completion.create(
+        engine="text-davinci-002",
+        prompt=question,
+        temperature=0.5,
+        max_tokens=1024
+    )
+elif day == 6:
+    # today is sunday 
+    # sundays are for rest and reset
+    quote = "Share a quote about love and attribute its author."
+    question = "Give me some life advice, in particular about raising daughters."
+
+    # Ask ChatGPT your question
+    quote_response = openai.Completion.create(
+        engine="text-davinci-002",
+        prompt=quote,
+        temperature=0.5,
+        max_tokens=1024
+    )
+
+
+    # Ask ChatGPT your question
+    question_response = openai.Completion.create(
+        engine="text-davinci-002",
+        prompt=question,
+        temperature=0.5,
+        max_tokens=1024
+    )
+else:
+    quote_response = 'error yo'
+    question_response = 'error yo'
+
 
 # Send the response to the incoming Slack webhook
 slack_response = client.send(
-    text="fallback",
+    text="good morning mr. pelkey...",
     blocks=[
         {
             "type": "header",
             "text": {
                 "type": "plain_text",
-                "text": ":robot_face: :computer: :game_die:  Good Morning Mr. Pelkey  :game_die: :computer: :robot_face:"
+                "text": ":robot_face: :game_die:  Good Morning Mr. Pelkey  :game_die: :robot_face:"
             }
         },
         {
             "type": "context",
             "elements": [
                 {
-                    "text": "_On this day " + str(random_num) + " years ago..._",
-                    "type": "mrkdwn"
+                    "type": "mrkdwn",
+                    "text": quote_response["choices"][0]["text"]
                 }
             ]
         },
@@ -68,7 +218,7 @@ slack_response = client.send(
             "type": "section",
             "text": {
                 "type": "mrkdwn",
-                "text": chat_response["choices"][0]["text"]
+                "text": question_response["choices"][0]["text"]
             }
         },
         {
@@ -81,8 +231,9 @@ slack_response = client.send(
                     "text": "_I have been alive for " + str(uptime_days) + " days, " \
                             + str(uptime_hours) + " hours, " + str(uptime_minutes) \
                             + " minutes, and " + str(uptime_seconds) + " seconds || " \
-                            + str(cpu_percent) + "% cpu in use || " \
-                            + str(memory_percent) + "% ram in use_",
+                            + str(cpu_percent) + "% cpu || " \
+                            + str(memory_percent) + "% ram || " \
+                            + str(disk_usage.percent) + "% disk_",
                     "type": "mrkdwn"
                 }
             ]
